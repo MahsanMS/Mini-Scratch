91137018fdf873f813e407616e0499b32620df1a
MahsanMS <mahsan.ms1386@gmail.com>
2026-02-08 12:48:03 +0330

    Change TextBox to InputBox and add version 1 of Block structure

--- UI.h
+++ UI.h
@@ -3,26 +3,26 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL2_gfx.h>
 
-struct TextBox
+struct InputBox
 {
-    Sint16 x, y, w, h;
+    Sint16 x, y;
+    Sint16 rel_x, rel_y, w, h;
     bool active = false;
     bool hover = false;
 
-    vector<string> lines;
-    string current_line;
+    TextItem line;
 
     int cursor_counter = 0;
 
-    // colors of textbox
+    // colors of inputbox
     Uint8 box_r = 255, box_g = 255, box_b = 255, box_a = 255;
     Uint8 text_r = 255, text_g = 255, text_b = 255, text_a = 255;
     Uint8 active_r = 0, active_g = 255, active_b = 0, active_a = 255;
     Uint8 hover_r = 128, hover_g = 128, hover_b = 128, hover_a = 255;
 
-    TextBox(Sint16 X, Sint16 Y, Sint16 W, Sint16 H)
+    InputBox()
     {
-        x = X; y = Y; w = W; h = H;
+        w = 10, h = 27;
     }
 
     void set_box_RGBA(Uint8 r, Uint8 g, Uint8 b, Uint8 a)
@@ -45,48 +45,45 @@ struct TextBox
         hover_r = r; hover_g = g; hover_b = b; hover_a = a;
     }
 
-    void manage(const SDL_Event &e)
+    bool manage_event(const SDL_Event &e)
     {
         if (e.type == SDL_MOUSEBUTTONDOWN)
         {
             Sint16 mx = e.button.x;
             Sint16 my = e.button.y;
-            active = x <= mx && mx <= x + w && y <= my && my <= y + h;
+            active = x <= mx && mx <= x + w && y <= my
+            && my <= y + h;
         }
 
-        if (!active)
+        if (e.type == SDL_MOUSEMOTION)
         {
-            Sint16 mx = e.button.x;
-            Sint16 my = e.button.y;
-            hover = x <= mx && mx <= x + w && y <= my && my <= y + h;
-            return;
+            Sint16 mx = e.motion.x;
+            Sint16 my = e.motion.y;
+            hover = x <= mx && mx <= x + w && y <= my
+            && my <= y + h;
         }
+
+        if (hover) return true;
+        if (!active) return false;
 
         if (e.type == SDL_TEXTINPUT)
-            current_line += e.text.text;
+            line.text += e.text.text;
 
         else if (e.type == SDL_KEYDOWN)
         {
             if (e.key.keysym.sym == SDLK_BACKSPACE)
             {
-                if (!current_line.empty())
-                    current_line.pop_back();
-                else if (!lines.empty())
+                if (!line.text.empty())
                 {
-                    current_line = lines.back();
-                    lines.pop_back();
+                    line.text.pop_back();
                 }
             }
-
-            else if (e.key.keysym.sym == SDLK_RETURN)
-            {
-                lines.push_back(current_line);
-                current_line.clear();
-            }
         }
+
+        return true;
     }
 
-    void render(SDL_Renderer *renderer)
+    void render(SDL_Renderer *renderer, TTF_Font *font)
     {
         if (active)
             rectangleRGBA(renderer, x, y, x + w, y + h, active_r, active_g, active_b, active_a);
@@ -95,21 +92,43 @@ struct TextBox
         else
             rectangleRGBA(renderer, x, y, x + w, y + h, box_r, box_g, box_b, box_a);
 
-        const Sint16 text_x = x + 10;
-        Sint16 text_y = y + 10;
+        cursor_counter = (cursor_counter + 1) % 45;
 
-        for (auto l : lines)
+        TTF_SizeUTF8(font, line.text.c_str(), &line.w, &line.h);
+
+        w = line.w + 10;
+
+        if (line.text == "" && active && cursor_counter <= 25)
         {
-            stringRGBA(renderer, text_x, text_y, l.c_str(), text_r, text_g, text_b, text_a);
-            text_y += 20;
+            SDL_Rect cursor = {x + 5, y + 5, 2, line.h};
+            SDL_SetRenderDrawColor(renderer, text_r, text_g, text_b, text_a);
+
+            SDL_RenderFillRect(renderer, &cursor);
         }
 
-        string temp = current_line;
-        if (active && cursor_counter <= 25) temp += "|";
+        else
+        {
+            SDL_Surface* text_surface = TTF_RenderUTF8_Blended(font, line.text.c_str(), SDL_Color{text_r, text_g, text_b,
+                text_a});
 
-        stringRGBA(renderer, text_x, text_y, temp.c_str(), text_r, text_g, text_b, text_a);
+            if (!text_surface) return;
 
-        cursor_counter = (1 + cursor_counter) % 45;
+            SDL_Texture* text_texture = SDL_CreateTextureFromSurface(renderer, text_surface);
+
+            SDL_Rect text_dst = {x + 5, y + 5, text_surface -> w, text_surface -> h};
+            SDL_RenderCopy(renderer, text_texture, nullptr, &text_dst);
+
+            SDL_FreeSurface(text_surface);
+            SDL_DestroyTexture(text_texture);
+
+            if (active && cursor_counter <= 25)
+            {
+                SDL_Rect cursor = {x + w - 5, y + 5, 2, line.h};
+                SDL_SetRenderDrawColor(renderer, text_r, text_g, text_b, text_a);
+
+                SDL_RenderFillRect(renderer, &cursor);
+            }
+        }
     }
 };
 
@@ -163,7 +182,7 @@ struct Button
         }
     }
 
-    void render_button(SDL_Renderer *renderer)
+    void render(SDL_Renderer *renderer)
     {
         if (active)
             roundedBoxRGBA(renderer, x, y, x + w, y + h, radius, active_r, active_g, active_b, active_a);
@@ -171,6 +190,197 @@ struct Button
             roundedBoxRGBA(renderer, x, y, x + w, y + h, radius, hover_r, hover_g, hover_b, hover_a);
         else
             roundedBoxRGBA(renderer, x, y, x + w, y + h, radius, button_r, button_g, button_b, button_a);
+    }
+};
+
+struct TextItem {
+    string text;
+    int w, h;
+};
+
+struct Block
+{
+    Sint16 x, y;
+    Sint16 ghost_x, ghost_y;
+    int width, height;
+    int offset_x, offset_y;
+    bool dragging = false;
+
+    string look;
+    // 1 -> text
+    // 2 -> input
+
+    Uint8 block_r = 255, block_g = 255, block_b = 255, block_a = 255;
+    Uint8 ghost_r = 128, ghost_g = 128, ghost_b = 128, ghost_a = 255;
+    Uint8 text_r = 0, text_g = 0, text_b = 0, text_a = 255;
+
+    vector<TextItem> texts;
+    vector<InputBox> inputs;
+
+    Block(Sint16 _x, Sint16 _y)
+    {
+        x = _x; y = _y; height = 20; width = 10;
+    }
+
+    void set_block_RGBA(Uint8 r, Uint8 g, Uint8 b, Uint8 a)
+    {
+        block_r = r; block_g = g; block_b = b; block_a = a;
+    }
+
+    void set_ghost_RGBA(Uint8 r, Uint8 g, Uint8 b, Uint8 a)
+    {
+        ghost_r = r; ghost_g = g; ghost_b = b; ghost_a = a;
+    }
+
+    void set_text_RGBA(Uint8 r, Uint8 g, Uint8 b, Uint8 a)
+    {
+        text_r = r; text_g = g; text_b = b; text_a = a;
+    }
+
+    void set_inputs_box_RGBA(Uint8 r, Uint8 g, Uint8 b, Uint8 a)
+    {
+        for (auto& inp: inputs)
+            inp.set_box_RGBA(r, g, b, a);
+    }
+
+    void set_inputs_text_RGBA(Uint8 r, Uint8 g, Uint8 b, Uint8 a)
+    {
+        for (auto& inp: inputs)
+            inp.set_text_RGBA(r, g, b, a);
+    }
+
+    void set_inputs_active_RGBA(Uint8 r, Uint8 g, Uint8 b, Uint8 a)
+    {
+        for (auto& inp: inputs)
+            inp.set_active_RGBA(r, g, b, a);
+    }
+
+    void set_inputs_hover_RGBA(Uint8 r, Uint8 g, Uint8 b, Uint8 a)
+    {
+        for (auto& inp: inputs)
+            inp.set_hover_RGBA(r, g, b, a);
+    }
+
+    void add_text(string t, TTF_Font* font)
+    {
+        TextItem item;
+        item.text = t;
+        TTF_SizeUTF8(font, t.c_str(), &item.w, &item.h);
+
+        texts.push_back(item);
+
+        width += item.w;
+        width += 5;
+        height = max(height, item.h + 10);
+
+        look += '1';
+    }
+
+    void add_input()
+    {
+        InputBox inp_box;
+        inp_box.rel_x = width;
+        inp_box.rel_y = 5;
+
+        inputs.push_back(inp_box);
+
+        width += inp_box.w;
+        width += 5;
+        height = max(height, inp_box.h + 10);
+
+        look += '2';
+    }
+
+    void manage_event(const SDL_Event& e)
+    {
+        bool new_input = false;
+        for (auto& it: inputs)
+            new_input |= it.manage_event(e);
+
+        if (new_input) return;
+
+        if (e.type == SDL_MOUSEBUTTONDOWN)
+        {
+            int mx = e.button.x;
+            int my = e.button.y;
+
+            ghost_x = x;
+            ghost_y = y;
+
+            if (x <= mx && mx <= x + width && y <= my && my <= y + height)
+            {
+                dragging = true;
+                offset_x = mx - ghost_x;
+                offset_y = my - ghost_y;
+            }
+        }
+
+        if (e.type == SDL_MOUSEMOTION && dragging)
+        {
+            ghost_x = e.motion.x - offset_x;
+            ghost_y = e.motion.y - offset_y;
+        }
+
+        if (e.type == SDL_MOUSEBUTTONUP)
+        {
+            dragging = false;
+            x = ghost_x;
+            y = ghost_y;
+        }
+    }
+
+    void render(SDL_Renderer* renderer, TTF_Font* font)
+    {
+        boxRGBA(renderer, x, y, x + width, y + height, block_r, block_g, block_b, block_a);
+
+        int x2 = x + 5;
+        int text_counter = 0;
+        int input_counter = 0;
+        int new_width = 5;
+
+        for (char ch: look)
+        {
+            if (ch == '1')
+            {
+                TextItem &t = texts[text_counter++];
+                SDL_Color color = {text_r, text_g, text_b, text_a};
+
+                SDL_Surface* s = TTF_RenderUTF8_Blended(font, t.text.c_str(), color);
+
+                SDL_Texture* tex = SDL_CreateTextureFromSurface(renderer, s);
+
+                SDL_Rect dst = { x2, y + 5, s->w, s->h };
+                SDL_RenderCopy(renderer, tex, nullptr, &dst);
+
+                x2 += t.w + 5;
+
+                SDL_FreeSurface(s);
+                SDL_DestroyTexture(tex);
+
+                new_width += t.w + 5;
+            }
+
+            else if (ch == '2')
+            {
+                InputBox& inp = inputs[input_counter++];
+
+                inp.x = x2;
+                inp.y = y + inp.rel_y;
+
+                inp.render(renderer, font);
+
+                new_width += inp.w + 5;
+                x2 += inp.w + 5;
+            }
+        }
+
+        width = new_width + 5;
+
+        if (dragging)
+        {
+            boxRGBA(renderer, ghost_x, ghost_y, ghost_x + width, ghost_y + height, ghost_r, ghost_g,
+                ghost_b, ghost_a);
+        }
     }
 };
 
